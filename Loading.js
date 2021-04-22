a49575b52952d8d3e4cd9357c6b4eb852315c5a0
Hwang-yoonji <kaci00@naver.com>
2021-04-22 19:44:37 +0900

    로딩화면

---
+++ Loading.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function Loading(){
+    return (
+        <View style={styles.container}>
+            <Text style={styles.text}>SaveTime_APP</Text>
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: "flex-end",
+        paddingHorizontal: 30,
+        paddingVertical: 100,
+        backgroundColor: "#b4ffff"
+    },
+    text: {
+        color: "black",
+        fontSize: 20
+    }
+});
