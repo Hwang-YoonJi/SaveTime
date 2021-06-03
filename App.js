b2ddab6a989c0b97279ea32bcdbdc61029e00518
Hwang-yoonji <kaci00@naver.com>
2021-06-03 16:31:01 +0900

    사용권한요청

--- App.js
+++ App.js
@@ -1,16 +1,29 @@
-import React from 'react';
+import React from "react";
+import {Alert} from "react-native";
 import Loading from "./Loading";
 import * as Location from "expo-location";
 
 export default class extends React.Component {
-  getLocation = async() => {
-    const location = await Location.getCurrentPositionAsync();
-    console.log(location);
-  }
+  state = {
+    isLoading: true
+  };
+  getLocation = async () => { //함수생성
+    try {
+      await Location.requestPermissionsAsync(); //권한 인증받기
+      const { 
+        coords: {latitude, longitude } 
+      } = await Location.getCurrentPositionAsync(); //지역정보 받아오기
+      //api로 전송하고 날씨가져오기
+      this.setState({ isLoading: false });
+    } catch (error) {
+      Alert.alert("Can't find you", "So sad"); //사용자가 거절하면 뜨는 메시지 (try블럭에서 에러생기면 catch블럭실행)
+    }
+  };
   componentDidMount(){
     this.getLocation();
   }
   render() {
-    <Loading />;
+    const { isLoading } = this.state;
+    retrun; isLoading ? <Loading /> : null; //isLoading이 있으면 <Loading />을 리턴하고, 아니면 null 리턴
   }
 }
